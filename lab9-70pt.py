86a0168f2f79f24bbcb2356fae6c7d23d114ed9c
dangeryoder <dangeryoder@gmail.com>
2014-10-16 14:57:54 -0500

    70 pt done!

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -12,3 +12,10 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+
+print "Give me a celcius temperature to convert to farenheit"
+celcius = int(raw_input())
+celcius = celcius * 9
+celcius = celcius / 5
+celcius = celcius + 32
+print str(celcius) + " degrees farenheit"
